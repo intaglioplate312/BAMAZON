ee71e0b0b3c4c6f8257e39c27eb6c9aab7920c10
intaglioplate312 <helenelizabeth@me.com>
2017-08-06 17:28:48 -0500

    quantity funcitonality

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -1,37 +1,35 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
-var colors = require('colors');
-
 var connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: '',
     database: 'bamazondb'
 });
 
+// connect to bamazon database
 connection.connect(function(err) {
-    if (err) {
-        console.log(err);
-    }
-    displayStock();
-    //console.log("you are connected");
-});
 
-//Getting data from db
-function displayStock() {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+});
+BuyBooks();
+
+function buyBooks() {
+    // display all of the items that are for sale: IDs, names, and price
     connection.query('SELECT * FROM products', function(err, rows) {
         if (err) throw err;
         //book list formatting
+        console.log("Welcome to Helen’s Idiosyncratic Book Shop");
         console.log("\nID \tNAME\t\t\t\t\t\t PRICE");
         for (var i = 0; i < rows.length; i++) {
             console.log(rows[i].item_id + '\t' + rows[i].product_name + '\t$ ' + rows[i].price + '\n');
             // Poormina can I align on decimal with \t unable to find google answer
         };
     });
-    buyBook();
-};
 
-function buyBook() {
+    // prompt user to ask them the ID of the product they would like to buy
     inquirer.prompt([{
         type: "list",
         name: "option",
@@ -41,43 +39,45 @@ function buyBook() {
             "Yes. I would",
             "No Thanks, I have enough to read",
         ]
-    }]).then(function(user) {
+    }]).then(function(answer) {
         //if no end program
-        if (user.option === 'No Thanks, I have enough to read') {
+        if (answer.option === 'No Thanks, I have enough to read') {
             console.log("Thank you have a nice day");
+
         } else {
-            // if yes ask item id of book = customerChoiceID
+            // if yes ask item ID (id, ego, super ego) of book = customerChoiceID
             inquirer.prompt([{
                     type: "input",
-                    name: "customerChoiceID",
-                    message: "Please enter the ID number of the product you would like to buy?",
+                    name: "ego",
+                    message: "Please enter the ID number of the product you would like to buy?"
+                        //add validation if time allows
+                        // yes ask number of copies wanted= customerChoiceQuantiy
                 },
-                // yes ask number of copies wanted= customerChoiceQuantiy
                 {
                     type: "input",
-                    name: "customerChoiceQuantiy",
-                    message: "How many units would you like to buy?"
-                },
-            ]).then(function(user) {
-                connection.query('SELECT * FROM products WHERE item_id =?' = [customerChoiceID], function(err, rows) {
+                    name: "amount",
+                    message: "How many books would you like to buy?"
+                        //add validation if time allows
+                }
+            ]).then(function(answers) {
+                console.log(answers.ego, answers.amount)
+                    //security issue??  
+                connection.query('SELECT * FROM Products WHERE item_id = ?', [answers.ego], function(err, res) {
                     if (err) throw err;
-                    console.log(rows[i].item_id + '\t' + rows[i].product_name + '\t$ ' + rows[i].price + '\n');
+
+                    // Check stock_qty
+                    if (answers.amount > res[0].stock_qty) {
+                        console.log("Sorry that item is currently out of stock" + '\n' + '\n');
+
+                        buyBooks();
+                    }
+                    // Price to customer 100% markup
 
                 })
-
-                //console.log(user.customerChoiceID, user.customerChoiceQuantiy);
             });
-
-
-
-
-
-
-
         }
     })
-}
-
+};
 
 
 
